204a63052b8c062f9a5472d5db2307cab237358d
Venkat <venkata@venkatamutyala.com>
2025-10-15 19:41:12 +0000

    fixes

--- vault/scripts/install_vault.sh
+++ vault/scripts/install_vault.sh
@@ -3,8 +3,8 @@ echo "BUILT IN OPENBAO_VERSION: ${CACHED_OPENBAO_VERSION}"
 PROVIDED_OPENBAO_VERSION="$1" # Get the Vault version from the first command-line argument
 
 cd ~
-if [ "$CACHED_OPENBAO_VERSION" != "$PROVIDED_VERSION" ]; then
-    echo "Removing cached bao and installing: ${PROVIDED_VERSION}"
+if [ "$CACHED_OPENBAO_VERSION" != "$PROVIDED_OPENBAO_VERSION" ]; then
+    echo "Removing cached bao and installing: ${PROVIDED_OPENBAO_VERSION}"
     rm -f /usr/bin/bao
     wget -O https://github.com/openbao/openbao/releases/download/v${PROVIDED_OPENBAO_VERSION}/bao_${PROVIDED_OPENBAO_VERSION}_Linux_x86_64.tar.gz /tmp/bao_${PROVIDED_OPENBAO_VERSION}_Linux_x86_64.tar.gz
     tar -xzvf /tmp/bao_${PROVIDED_OPENBAO_VERSION}_Linux_x86_64.tar.gz bao && mv bao /usr/bin/bao && rm /tmp/bao_${PROVIDED_OPENBAO_VERSION}_Linux_x86_64.tar.gz
